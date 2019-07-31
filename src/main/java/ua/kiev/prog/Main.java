b53a3ab852c0c34b05bb38a04024000ec2b9a2b8
Andrey Chernov <andrii.chernov@gmail.com>
2019-07-31 16:17:47 +0300

    2

--- src/main/java/ua/kiev/prog/Main.java
+++ src/main/java/ua/kiev/prog/Main.java
@@ -2,11 +2,6 @@ package ua.kiev.prog;
 
 public class Main {
   
-  public static void main(String[] args) {
-<<<<<<< HEAD
-    System.out.println("Main.main");
-    //deleted
-=======
->>>>>>> 51fabfe6fa5436c48de72c13516427e56af87c60
+  public static void main(String[] args) {  
   }
 }
