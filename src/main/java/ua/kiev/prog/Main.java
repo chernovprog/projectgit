063a7a966dc24060d0051726e745c2cf004cdecf
Andrey Chernov <andrii.chernov@gmail.com>
2019-07-31 16:15:14 +0300

    1

--- src/main/java/ua/kiev/prog/Main.java
+++ src/main/java/ua/kiev/prog/Main.java
@@ -5,7 +5,7 @@ public class Main {
   public static void main(String[] args) {
 <<<<<<< HEAD
     System.out.println("Main.main");
-    //deleted 
+    //deleted
 =======
 >>>>>>> 51fabfe6fa5436c48de72c13516427e56af87c60
   }
